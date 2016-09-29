f64ed4d8b62a4f17b393b460bb8c2ef795e5fa11
Roxy <Roxy@DESKTOP-336JRR4>
2016-09-29 09:37:32 +0200

    eh

--- TicTacToe.UnitTests/GameWinnerServiceTests.cs
+++ TicTacToe.UnitTests/GameWinnerServiceTests.cs
@@ -2,16 +2,10 @@
 
 namespace TicTacToe.UnitTests
 {
-	[TestFixture]
-	public class GameWinnerServiceTests {
-		[Test]
-		public void NeitherPlayerHasThreeInARow() {
-			const char expected = ' ';
-			var gameBoard = new char[3, 3] { {' ', ' ', ' '},
-											{' ', ' ', ' '},
-											{' ', ' ', ' '}};
-			var actual = gameWinnerService.Validate(gameBoard);
-			Assert.AreEqual(expected, actual);
-		}
-	}
+    public class Class1GameWinnerServiceTests {
+        public GameWinnerServiceTests()
+        {
+
+        }
+    }
 }
