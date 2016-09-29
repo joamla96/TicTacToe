a987e268fc9de8ea04a53c21a8d809c4d7ffc7dd
jona8690 <jona8690@edu.eal.dk>
2016-09-29 09:36:32 +0200

    Test

--- TicTacToe.UnitTests/GameWinnerServiceTests.cs
+++ TicTacToe.UnitTests/GameWinnerServiceTests.cs
@@ -5,6 +5,7 @@ namespace TicTacToe.UnitTests
 	[TestFixture]
 	public class GameWinnerServiceTests {
 		[Test]
+
 		public void NeitherPlayerHasThreeInARow() {
 			const char expected = ' ';
 			var gameBoard = new char[3, 3] { {' ', ' ', ' '},
